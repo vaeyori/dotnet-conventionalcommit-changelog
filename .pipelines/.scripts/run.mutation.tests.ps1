e2ad660f37e5d3251e89d0237f535bef00da769d
Joshua (ysovuka) Thompson <ysovuka@gmail.com>
2021-01-17 23:40:58 -0600

    build: Revert powershell command

--- .pipelines/.scripts/run.mutation.tests.ps1
+++ .pipelines/.scripts/run.mutation.tests.ps1
@@ -152,7 +152,7 @@ function GenerateReports($outputPath, $cd)
         # Invoke all commands.
         foreach($command in $commands)
         {
-            $("$dotnetStrykerPath") -p "$($command)" --abort-test-on-fail --threshold-high 99 --threshold-low 90 --threshold-break 85 --mutation-level 'Advanced' --reporters "['json', 'html', 'progress']"
+            & $dotnetStrykerPath -p "$($command)" --abort-test-on-fail --threshold-high 99 --threshold-low 90 --threshold-break 85 --mutation-level 'Advanced' --reporters "['json', 'html', 'progress']"
 
             if ($_.Exception){
                 throw $_.Exception
